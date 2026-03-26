d552442c6b1630d7d20245ab2d3f10c14373ff8b
Sarthak Khare <138134731+Sarthakiee@users.noreply.github.com>
2026-03-26 18:51:22 +0530

    Time: 1 ms (88.37%), Space: 44 MB (83.85%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> powerset = new ArrayList<>();
+        int n = nums.length;
+        int total = 1 << n;
+        for(int i = 0; i < total; i++){
+            List<Integer> subset = new ArrayList<>();
+            for(int j = 0; j < n; j++){
+                if((i & (1 << j))!=0){
+                    subset.add(nums[j]);
+                }
+            }
+
+            powerset.add(subset);
+        }
+
+        return powerset;
+    }
+}
